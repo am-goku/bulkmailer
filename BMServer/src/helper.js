7ae2af6f2c6e35f77a981b4ab7523c6c39e6a023
Gokul Krishna <amkrishna2483@gmail.com>
2024-06-12 21:26:59 +0530

    1. ntergrated email validation using abract
    2. Now the recipient emails from client will be filtered before sending a message
    
    @am-goku
    @date: 12th June 2024

--- BMServer/src/helper.js
+++ BMServer/src/helper.js
@@ -1,9 +1,19 @@
+import validateEmailAddress from "./services/mailValidator.js";
 import sendMail from "./services/nodemailer.js";
 
 export const sendMailHelper = async (account, mailData, recipients) => {
-    return new Promise((resolve, reject) => {
-        sendMail(account, mailData, recipients).then((response) => {
-            return resolve({ status: 200, message: "Email sent successfully", info: response });
+    return new Promise(async (resolve, reject) => {
+
+        const filteredEmail = await validateEmailAddress(recipients);
+
+        const {accepted, rejected} = filteredEmail;
+
+        sendMail(account, mailData, accepted).then((response) => {
+            const info = {
+                ...response,
+                ['rejected']: [...response.rejected, ...rejected],
+            }
+            return resolve({ status: 200, message: "Email sent successfully", info: info });
         }).catch((error) => {
             return reject({ status: 500, message: "Error sending mail", error: error });
         })
