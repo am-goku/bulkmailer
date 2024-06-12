aa4100e7d2683cc99f824fe30ac03f54fbaca41a
Gokul Krishna <amkrishna2483@gmail.com>
2024-06-12 18:37:23 +0530

    1. Updated export section
    2. Now the user can choose and download the status of emails has been sent
    
    @am-goku
    @date: 12th June 2024

--- BMClient/src/scripts/extractor.js
+++ BMClient/src/scripts/extractor.js
@@ -30,3 +30,67 @@ export const parseDataFile = async (file) => {
         }
     });
 }
+
+
+
+
+
+
+
+
+
+
+export const convertToExcel = (JSONData) => {
+    return new Promise((resolve, reject) => {
+        try {
+            const jsonData = JSONData.accepted.map(email => ({
+                email: email,
+                status: 'Accepted'
+            }));
+
+            jsonData.push(
+                JSONData.rejected.map(email => ({
+                    email: email,
+                    status: 'Rejected'
+                })
+                ))
+
+            // Add summary row
+            jsonData.push(
+                { email: 'Total', status: JSONData.total },
+                { email: 'Success', status: JSONData.success },
+                { email: 'Rejected', status: JSONData.failed },
+                { email: 'Percentage', status: JSONData.percentage },
+                { email: 'Status', status: JSONData.status }
+            );
+
+            const workbook = XLSX.utils.book_new();
+
+            const worksheet = XLSX.utils.json_to_sheet(jsonData);
+
+            XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+
+            const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+
+            const data = new Blob([excelBuffer], { type: 'application/octet-stream' });
+
+            const link = document.createElement('a');
+
+            const url = URL.createObjectURL(data);
+
+            link.href = url;
+            link.download = `SampleData.xlsx`;
+
+            document.body.appendChild(link);
+
+            link.click();
+
+            document.body.removeChild(link);
+
+            resolve();
+
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
